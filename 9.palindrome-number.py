383ece0e0d9490805b23deb5918cf7c34f8b77f5
KisoreAbhinav <kisoreabhinav@gmail.com>
2025-12-05 13:10:50 +0530

    Palindrome Number Solution

---
+++ 9.palindrome-number.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=9 lang=python
+#
+# [9] Palindrome Number
+#
+
+# @lc code=start
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if(x<0):
+            return False
+        rev = 0
+        num =x
+        while (num!=0):
+            rev=rev*10+num%10
+            num=num//10
+        return rev==x
+# @lc code=end
+
